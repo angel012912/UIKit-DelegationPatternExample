cf03965973d94d9acadbdd038b69966125b79c3d
angel012912 <angel_012912@hotmail.com>
2023-07-05 13:14:59 -0600

    Initial Commit

---
+++ DelegationPatternExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DelegationPatternExample
+//
+//  Created by Angel Garcia on 05/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
